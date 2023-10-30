bbe82548635a4590be1ed4dde0371b6b91ef10a8
AnMal <junkie_@msn.com>
2023-10-30 21:29:22 +0100

    Add project files.

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+    public class BookingController : Controller
+    {
+        private static readonly List<HotelBooking> _bookings = new();
+
+        public IActionResult Index()
+        {
+            return View(_bookings);
+        }
+
+        public IActionResult Create()
+        {
+            HotelBooking hotelBooking = new();
+            return View(hotelBooking);
+        }
+
+        public IActionResult CreateBooking(HotelBooking hotelBookingModel)
+        {
+            hotelBookingModel.Id = _bookings.Count + 1;
+            _bookings.Add(hotelBookingModel);
+            return RedirectToAction("Index");
+        }
+    }
+}
